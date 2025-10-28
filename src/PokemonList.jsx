e900020b0831e0218952c9cf12a218c4bbc7e554
Kateryna Dobrovolska <k.dobrovolska@sellerlogic.com>
2025-10-28 19:09:47 -0400

    add heder Pokermons

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -3,20 +3,23 @@ import { Link } from "react-router-dom";
 
 const PokemonList = ({ pokemonList }) => {
   return (
-    <div className="list-container">
-      {pokemonList.map(({ id, name }) => (
-        <Link
-          key={id}
-          to={`/pokemon/${name}`}
-          className="list-item"
-          style={{
-            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
-          }}
-        >
-          <div className="list-item-name">{name}</div>
-        </Link>
-      ))}
-    </div>
+    <>
+      <h1>Pokermons:</h1>
+      <div className="list-container">
+        {pokemonList.map(({ id, name }) => (
+          <Link
+            key={id}
+            to={`/pokemon/${name}`}
+            className="list-item"
+            style={{
+              backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
+            }}
+          >
+            <div className="list-item-name">{name}</div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
