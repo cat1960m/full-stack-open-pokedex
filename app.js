91d63444442e44a4facded2dd58754ce9640f4fe
Kateryna Dobrovolska <k.dobrovolska@sellerlogic.com>
2025-10-27 22:52:09 -0400

    health error

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
-app.get('/health', (req, res) => {
-  res.status(200).send('OK');
+app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw "error...  ";
+  res.status(200).send("OK");
 });
 
 app.listen(PORT, () => {
