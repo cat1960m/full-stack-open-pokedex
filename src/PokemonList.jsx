660ea75a50bb54365c8243010c93dc8f78bfd26c
Kateryna Dobrovolska <k.dobrovolska@sellerlogic.com>
2025-10-27 22:41:36 -0400

    add health

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -3,23 +3,20 @@ import { Link } from "react-router-dom";
 
 const PokemonList = ({ pokemonList }) => {
   return (
-    <>
-      <h1>pokemons</h1>
-      <div className="list-container">
-        {pokemonList.map(({ id, name }) => (
-          <Link
-            key={id}
-            to={`/pokemon/${name}`}
-            className="list-item"
-            style={{
-              backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
-            }}
-          >
-            <div className="list-item-name">{name}</div>
-          </Link>
-        ))}
-      </div>
-    </>
+    <div className="list-container">
+      {pokemonList.map(({ id, name }) => (
+        <Link
+          key={id}
+          to={`/pokemon/${name}`}
+          className="list-item"
+          style={{
+            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})`,
+          }}
+        >
+          <div className="list-item-name">{name}</div>
+        </Link>
+      ))}
+    </div>
   );
 };
 
